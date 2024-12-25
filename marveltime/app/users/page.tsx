22754aabf3bc8186ae7d9a6c72ee62cd89b35370
shiba517 <shiba.cdeb@gmail.com>
2024-12-25 22:21:39 +0000

    NEXTJS installation with page and sub page example

---
+++ marveltime/app/users/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const UsersPage = () => {
+  return (
+    <div>Users page</div>
+  )
+}
+
+export default UsersPage
---
+++ marveltime/app/users/players/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PlayersPage = () => {
+  return (
+    <div>Players Page</div>
+  )
+}
+
+export default PlayersPage
